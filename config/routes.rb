470680a6e2555cffb0965c200526cacf057f7137
Fei Wang <feiwangsioc@gmail.com>
2019-09-07 15:30:44 -0400

    add edit update functionality and partial

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,10 @@ get 'about', to: 'pages#about'
 
 resources :articles
 
+get 'signup', to: 'users#new'
+
+resources :users, except:[:new]
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
